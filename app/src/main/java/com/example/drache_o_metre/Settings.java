afb81dafb5dc9152bf2d294e7bfa8c551d3fd757
Killian T <killian.treuil@gmail.com>
2024-12-23 02:46:02 +0100

    Added SharedPreferences for both preferences

--- app/src/main/java/com/example/drache_o_metre/Settings.java
+++ app/src/main/java/com/example/drache_o_metre/Settings.java
@@ -1,6 +1,10 @@
 package com.example.drache_o_metre;
 
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -9,17 +13,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
 
 public class Settings extends AppCompatActivity {
 
     private Switch notificationSwitch;
     private Spinner freqSpinner;
     private Button back;
+    private Button moreButton;
+    private Button applySpinner;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -35,25 +42,47 @@ public class Settings extends AppCompatActivity {
                 R.array.freqRefresh,
                 android.R.layout.simple_spinner_item
         );
-        // Précisions sur l'affichage lorsqu'on clique sur le spinner
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         freqSpinner.setAdapter(adapter);
 
+        // Pour afficher l'élément en sharedPreference au devant
+        int pos = getFreqSpinnerPref();
+        freqSpinner.setSelection(pos);
+
+        //Initialisation du bouton apply pour le spinner
+        applySpinner = findViewById(R.id.applySpinner);
+
+        applySpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String spinnerString = (String)freqSpinner.getSelectedItem();
+
+                SharedPreferences sharedPref = getSharedPreferences("freqSpinner", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("freqSpinner", spinnerString);
+                editor.apply();
+            }
+        });
 
         //Initialisation de la switch pour les préférences de notification
         Switch notificationSwitch = findViewById(R.id.notificationSwitch);
 
+        // Si la préférence est oui, elle coche le switch.
+        if(isMeteoNotifSelected()) {
+            notificationSwitch.setChecked(true);
+        }
+
         notificationSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences sharedPref = getSharedPreferences("meteoNotification",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
                 if(notificationSwitch.isChecked()){
-                    //TODO : valeurs pour l'envoi de messages
-                    System.out.println("ok");
+                    editor.putString("meteoNotification", "true");
                 } else {
-                    //Supprimer les notifications de météo
-                    System.out.println("ko");
-
+                    editor.putString("meteoNotification", "false");
                 }
+                editor.apply();
             }
         });
 
@@ -69,16 +98,37 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-
+        // Ajout du bouton pour les informations supplémentaires.
+        Button moreButton = findViewById(R.id.moreButton);
+        moreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com/shorts/xkAr7jMtETI?si=W2Bwe8b9BGmgo4JH"));
+                startActivity(intent);
+            }
+        });
 
     }
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        parent.getItemAtPosition(pos);
 
-        //Pour récupérer les informations
+    public boolean isMeteoNotifSelected(){
+        // renvoie la val de météonotif en booleen
+        SharedPreferences sharedPref = getSharedPreferences("meteoNotification", MODE_PRIVATE);
+        String tmp = sharedPref.getString("meteoNotification", "");
+        if(tmp.equals("true")) {
+            return true;
+        }
+        return false;
     }
-    public void onNothingSelected(AdapterView<?> parent) {
-        //Another interface callback;
+
+    public int getFreqSpinnerPref() {
+        SharedPreferences sharedPref = getSharedPreferences("freqSpinner", MODE_PRIVATE);
+        String tmp = sharedPref.getString("freqSpinner", "");
+        if(tmp.equals("High")){
+            return 0;
+        } else if (tmp.equals("Balanced")) {
+            return 1;
+        }
+        return 2;
     }
 }
 
